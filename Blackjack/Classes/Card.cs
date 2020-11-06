808c83586d8ad09093bd27f37f4d4f7f3159b446
Spencer Pearson <spencer.pearson.816@outlook.com>
2020-11-06 13:49:35 -0600

    Build card and deck classes

---
+++ Blackjack/Classes/Card.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Classes;
+
+namespace Blackjack
+{
+    public class Card
+    {
+        //fields
+        //props
+        public Suit Suit { get; set; }
+        public Value Value { get; set; }
+        public int Score { get; set; }
+        public char Number { get; set; }
+        //ctors
+        public Card(Suit suit, Value value)
+        {
+            Suit = suit;
+            Value = value;
+
+
+        }
+        //methods
+    }
+}
